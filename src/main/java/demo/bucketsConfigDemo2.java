b70f82ec306ea352574fa71de7dd2b221776e89d
lxp <1606616774@qq.com>
2021-11-17 14:37:29 +0800

    新增获取回源配置2(应用场景)

--- src/main/java/demo/bucketsConfigDemo2.java
+++ src/main/java/demo/bucketsConfigDemo2.java
@@ -1,24 +1,27 @@
 package demo;
 
-import pojo.Bucket;
-import pojo.BucketConfig;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import pojo.Bucket;
+import pojo.BucketConfig;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- *获取客户账号下 CDN 服务回源配置
- * 其他字段可根据文档自定义,翻页参数需要手动输入
+ *获取客户账号下所有 CDN 服务回源配置 和应用场景
+ * 其他字段可根据文档自定义,默认数量 100 个服务单位,超过需要手动输入翻页参数
  *又拍云API文档：https://api.upyun.com/doc#/api/guide/overview
  *
  */
-public class bucketsConfigDemo {
+public class bucketsConfigDemo2 {
     public  static final String TOKEN = "";//用户token
     public  static final String TXT_PATH =System.getProperty("user.dir")+"/outputFile/configMES/bucketsconfig.txt";//配置信息输出文件位置
 
@@ -35,7 +38,7 @@ public class bucketsConfigDemo {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url("http://api.upyun.com/buckets?business_type=file&type=ucdn")
+                .url("http://api.upyun.com/buckets?business_type=file&type=ucdn&limit=400")
                 .method("GET", null)
                 .addHeader("Authorization", "Bearer "+TOKEN)
                 .build();
@@ -45,12 +48,29 @@ public class bucketsConfigDemo {
         JSONArray buckets = jsonObject.getJSONArray("buckets");
         if (buckets!=null){
             List<Bucket> bucketList=new ArrayList<Bucket>();
+            List<String> tagList = new ArrayList<String>();
             for (int i = 0; i < buckets.size(); i++) {
                 Bucket bucket=new Bucket();
                 List<String> bucketDomainList=new ArrayList<String>();
                 String o = buckets.get(i).toString();
                 JSONObject jsonObject1 = JSONObject.parseObject(o);
                 String bucket_name = jsonObject1.getString("bucket_name");
+                String tag = jsonObject1.getString("tag");
+                if (tag== null){
+                    bucket.setTag("默认");
+                }else if (tag.equals("picture")){
+                    bucket.setTag("网页图片");
+                }else if(tag.equals("download")){
+                    bucket.setTag("文件下载");
+                }else if(tag.equals("vod")){
+                    bucket.setTag("音视频点播");
+                }else if(tag.equals("dynamic")){
+                    bucket.setTag("动态内容");
+                }else if(tag.equals("fullSite")){
+                    bucket.setTag("全站加速");
+                }else{
+                    bucket.setTag("默认");
+                }
                 bucket.setBucketName(bucket_name);
                 JSONArray domains = jsonObject1.getJSONArray("domains");//获取服务下的域名
                 if (domains!=null){
@@ -136,6 +156,7 @@ public class bucketsConfigDemo {
         for (int i = 0; i < bucketList.size(); i++) {
             JSONObject jsonObject=new JSONObject();
             jsonObject.put("服务名",bucketList.get(i).getBucketName());
+            jsonObject.put("应用类型",bucketList.get(i).getTag());
             jsonObject.put("域名",bucketList.get(i).getDomainList());
             jsonObject.put("配置信息",bucketList.get(i).getConfigList());
             json.add(jsonObject);
--- src/main/java/pojo/Bucket.java
+++ src/main/java/pojo/Bucket.java
@@ -6,14 +6,22 @@ public class Bucket {
     private String bucketName;
     private List<String> domainList;//域名
     private List<BucketConfig> configList;//回源配置
+    private String tag; //应用类型
 
     public String getBucketName() {
-
         return bucketName;
     }
 
     public void setBucketName(String bucketName) {
         this.bucketName = bucketName;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
     }
 
     public List<String> getDomainList() {
@@ -38,6 +46,7 @@ public class Bucket {
                 "bucketName='" + bucketName + '\'' +
                 ", domainList=" + domainList +
                 ", configList=" + configList +
+                ", tag='" + tag + '\'' +
                 '}';
     }
 }
